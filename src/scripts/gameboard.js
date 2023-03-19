fab48e0a764f227580532acc0fd7384bb0e45e55
Sameer <sameer4363@gmail.com>
2023-03-19 09:20:53 -0400

    Add insertShip function and tests

--- src/scripts/gameboard.js
+++ src/scripts/gameboard.js
@@ -1,14 +1,43 @@
+import { ship } from './ship';
+
 export const gameboard = function (gridSize = 12) {
-  //   const board = [][];
-  //   for (let i = 0; i < gridSize; i++) {
-  //     for (let j = 0; j < gridSize; j++) {
-  //       board[i][j] = { hitStatus: false, containsShip: false };
-  //     }
-  //   }
-  const spaceObject = { hitStatus: false, containsShip: false };
+  const spaceObject = {
+    hitStatus: false,
+    containsShip: false,
+    shipIndex: null,
+  };
   const board = [...Array(gridSize)].map((e) =>
     Array(gridSize).fill(spaceObject)
   );
 
-  return { board };
+  const ships = [];
+
+  const insertShip = (coordinates) => {
+    coordinates.forEach((coordinate) => {
+      // Check if any coordinates are outside the board
+      if (coordinate[0] >= gridSize || coordinate[1] >= gridSize) {
+        throw new Error(
+          `[${coordinate[0]},${coordinate[1]}] is outside of grid size ${gridSize}`
+        );
+      }
+
+      // Check if any coordinates have a ship in them already
+      if (board[coordinate[0]][coordinate[1]].containsShip) {
+        throw new Error(
+          `[${coordinate[0]},${coordinate[1]}] is already occupied`
+        );
+      }
+    });
+
+    // Add ship to ships array
+    ships.push(ship(coordinates.length));
+
+    // Update contains ship status and index in ships array
+    coordinates.forEach((coordinate) => {
+      board[coordinate[0]][coordinate[1]].containsShip = true;
+      board[coordinate[0]][coordinate[1]].shipIndex = ships.length;
+    });
+  };
+
+  return { board, insertShip };
 };
--- src/scripts/tests/gameboard.test.js
+++ src/scripts/tests/gameboard.test.js
@@ -5,3 +5,47 @@ test('Create board with empty spaces', () => {
   expect(testBoard.board[0][0].hitStatus).toBeFalsy();
   expect(testBoard.board[11][11].containsShip).toBeFalsy();
 });
+
+test('Add ship to board', () => {
+  const testBoard = gameboard();
+  const coordinates = [
+    [1, 1],
+    [1, 2],
+    [1, 3],
+  ];
+  testBoard.insertShip(coordinates);
+  expect(testBoard.board[1][1].containsShip).toBeTruthy();
+  expect(testBoard.board[1][2].containsShip).toBeTruthy();
+  expect(testBoard.board[1][3].containsShip).toBeTruthy();
+});
+
+test('Throw error when ship added outside of board', () => {
+  const testBoard = gameboard(12);
+  const coordinates = [
+    [0, 11],
+    [0, 12],
+  ];
+
+  expect(() => testBoard.insertShip(coordinates)).toThrow(
+    '[0,12] is outside of grid size 12'
+  );
+});
+
+test('Throw error when ship added to occupied space', () => {
+  const testBoard = gameboard();
+  const coordinatesA = [
+    [1, 1],
+    [1, 2],
+    [1, 3],
+  ];
+  testBoard.insertShip(coordinatesA);
+
+  const coordinatesB = [
+    [1, 2],
+    [2, 2],
+  ];
+
+  expect(() => testBoard.insertShip(coordinatesB)).toThrow(
+    '[1,2] is already occupied'
+  );
+});
