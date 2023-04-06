4d7460fdac48ed0dc51c6f19a1e751ebda3fdd89
Sameer <sameer4363@gmail.com>
2023-04-06 18:33:32 -0400

    Add icon loading in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import './styles.css';
 import * as displayController from './scripts/displayController';
+import Icon from './images/github.svg';
+
+const githubIcon = new Image();
+githubIcon.src = Icon;
+githubIcon.classList.add('icon');
+githubIcon.alt = 'github icon';
+
+document.querySelector('body > footer > p > a').appendChild(githubIcon);
 
 // User will click load Game
 displayController.loadStartScreen();
